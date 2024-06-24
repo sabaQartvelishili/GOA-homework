c474abd5231d0f0f41198d01f144ebd83c355c26
sabaQartvelishili <qartvelishvilisaba83@mgail.com>
2024-06-25 02:02:49 +0530

    second_commit

---
+++ Day 0/lesson 2.py
@@ -0,0 +1,6 @@
+name = "Saba" #ეს არის str (string) ტიპის ცვლადი
+surname = "Qartvelishvili" #ეს არის str (string) ტიპის ცვლადი
+age = 15 #ეს არის int (integer) მთელი რიცხვი
+height = 170 #ეს აირს float ტიპის ცვლადი (მთელი რიცხვი)
+knows_programin = False #გამოიყენება True ან False
+print("me mqvia" + " " + name + " " + surname + " " + "me var" + " " + str(age) + " " + "wlis" + " " + "programirebis codna" + " " + str(knows_programin))
